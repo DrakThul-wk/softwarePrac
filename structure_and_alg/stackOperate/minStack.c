19d0c3bac95b13615c41f5beb1d9c73835580af8
wangkang <wangkang890419@163.com>
2021-09-14 22:34:20 +0800

    update min stack

--- structure_and_alg/stackOperate/minStack.c
+++ structure_and_alg/stackOperate/minStack.c
@@ -1,38 +1,64 @@
 #define MAXS 65536
 type struct {
-    int val[MAXS];
-    int top;
+    int val1[MAXS];
+    int top1;
+    int val2[MAXS];
+    int top2;
 }MinStack;
 
 /** initialize your data structure here */
 MinStack *minStackCreate() {
     MinStack *obj = (MinStack *)malloc(sizeof(MinStack));
-    obj->top = -1;
+    obj->top1 = -1;
+    obj->top2 = -1;
     return obj;
 }
 
 void minStackPush(MinStack *obj, int x) {
-    obj->top++;
-    obj->val[obj->top] = x;
+    obj->top1++;
+    obj->val1[obj->top1] = x;
+    if (obj->top2 == -1) {
+        obj->top2++;
+        obj->val2[obj->top2] = x;
+    } else {
+        if (x <= obj->val2[obj->top2]) {
+            obj->top2++;
+            obj->val2[obj->top2] = x;
+        }
+    }
 }
 
 void minStackPop(MinStack *obj) {
-    obj->top--;
+    if (obj->top1 == -1) {
+        return;
+    }
+    int valTmp = obj->val1[obj->top1];
+    obj->top1--;
+    if (obj->val2[obj->top2] == valTmp) {
+        obj->top2--;
+    }
 }
 
 int minStackTop(MinStack *obj) {
-    if (obj->top >= 0) {
-        return obj->val[obj->top];
+    if (obj->top1 >= 0) {
+        return obj->val1[obj->top1];
     }
     return -1;
 }
 
 int minStackGetMin(MinStack *obj) {
-
+    if (obj->top2 >= 0) {
+        return obj->val2[obj->top2];
+    }
+    return -1;
 }
 
 void minStackFree(MinStack *obj) {
-
+    if (obj == NULL) {
+        return;
+    }
+    free(obj);
+    obj = NULL;
 }
 
 /**
