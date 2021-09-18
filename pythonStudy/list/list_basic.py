fb2e6f01dad7f56501de27dab2201dd3b05b3f10
wangkang <wangkang890419@163.com>
2021-09-19 00:37:45 +0800

    update list operation

--- pythonStudy/list/list_basic.py
+++ pythonStudy/list/list_basic.py
@@ -7,3 +7,25 @@ print(bicycles[0])
 print(bicycles[0].title())
 
 print(bicycles[3].upper())
+# modification
+bicycles[0] = 'name'
+print(bicycles)
+
+# add to tail
+bicycles.append('ducati')
+print(bicycles)
+
+# insert to specific place
+bicycles.insert(0, 'wangkang')
+print(bicycles)
+
+bicycles.insert(1, 'wangkang')
+print(bicycles)
+
+del bicycles[0]
+print(bicycles)
+test_char = bicycles.pop(2)
+print(bicycles)
+
+bicycles.remove('redline')
+print(bicycles)
---
+++ pythonStudy/list/list_sort.py
@@ -0,0 +1,13 @@
+cars=[3,1,2,4]
+print(sorted(cars))
+print(cars)
+cars.sort()
+print(cars)
+
+cars_char = ['2','1','3','4']
+cars_char.sort()
+print('length of cars_char is:' + str(len(cars_char)))
+
+cars_char.reverse()
+print(cars_char)
+
