8c99afb8b7c687aa976ba87c942e446f44508439
wangkang <wangkang890419@163.com>
2021-10-18 22:52:01 +0800

    update class

--- pythonStudy/class/carClass.py
+++ pythonStudy/class/carClass.py
@@ -3,11 +3,27 @@ class Car():
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 0
 
     def get_descriptive_name(self):
         long_name = str(self.year) + ' ' + self.make + ' ' + self.model
         return long_name.title()
 
+    def update_odometer(self, mileage):
+        self.odometer_reading = mileage
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
 
 my_new_car = Car('LiXiang', 'ONE', 2021)
 print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
+my_new_car.increment_odometer(5)
+my_new_car.read_odometer()
+
